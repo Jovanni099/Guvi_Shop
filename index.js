40b36a6c45847d6930d3dc650b7649ceb3498e65
Jovanni099 <jovanni099tmb@gmail.com>
2026-01-17 13:54:56 +0500

    Создал Базовое приложение Express.
    Создал Базовое приложение Express. Применил метод listen. И использовал app.use(express.json())

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 /* index.js это точка входа в приложение */
+import express from 'express'
+
+app.use(express.json())
+
+const app = express()
+
+app.listen(4445, err => {
+    if(err){
+        console.log(err);
+    }
+    console.log('Server OK');
+    
+})
