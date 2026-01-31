4e8d55638291cc93752e9187f7391a0b5abbedbc
Jovanni099 <jovanni099tmb@gmail.com>
2026-01-31 11:41:34 +0500

    Подключение к mongo

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /* index.js это точка входа в приложение */
 import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+
+
+mongoose
+	.connect('mongodb+srv://Guvi_shop_adminnator:OERzJfnN2Vq512uG@cluster0.nit1wsy.mongodb.net')
+	.then(() => console.log('DB ok'))
+	.catch(err => console.log('DB error', err))
+
+
+const app = express()
 
 app.use(express.json())
 
-const app = express()
 
 app.listen(4445, err => {
     if(err){
@@ -12,3 +23,5 @@ app.listen(4445, err => {
     console.log('Server OK');
     
 })
+
+
